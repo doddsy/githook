e9e57dc1274085f591c34de6e276963d238f843d
doddsy <git@metrono.de>
2019-05-14 15:04:41 +0100

    :art: Ran this boi through Black. Mmm, so clean.

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, Response
+
 # Using json rather than Flask's jsonify because I couldn't figure it out and it made me wish to un-alive
 import json
 import requests
@@ -9,9 +10,13 @@ import sys
 # Just wanted to try something new tbh
 def configerror(type, key=None, extra=None):
     if type == "missing":
-        print(f'You are missing the {key} key from your config.py. Please recreate the file.')
+        print(
+            f'You are missing the {key} key from your config.py. Please recreate the file.'
+        )
     if type == "default":
-        print(f'You are using the default {key} value in your config.py. Please change this before running the program.')
+        print(
+            f'You are using the default {key} value in your config.py. Please change this before running the program.'
+        )
     if type == "undefined":
         print(extra)
 
@@ -19,7 +24,9 @@ def configerror(type, key=None, extra=None):
 def checkwebhook(url):
     r = requests.get(url).text
     # Check if webhook is even active
-    if all(keys in r for keys in ("name", "guild_id", "token")):
+    if all(
+        keys in r for keys in ("name", "guild_id", "token")
+    ):
         return True
 
 
@@ -35,23 +42,26 @@ def main():
             return Response(status=400)
     else:
         return Response(status=400)
+    # optional parameters
     if 'hideAuthor' in request.args:
         authorHidden = True
     else:
         authorHidden = False
-
-    # If incoming request headers don't match Gitlab's headers or the token is not matching
+    # If incoming request headers don't match Gitlab's headers
     if not request.headers.get('X-Gitlab-Event'):
         # 401 unauthorised
         return Response(status=401)
-
     # Get incoming json data (GL will only POST JSON)
     content = request.get_json()
     commits = []
     numOfCommits = 0
     repo = content["repository"]["name"]
     isPrivate = content["repository"]["visibility_level"]
-    branch = (":" + content["ref"].split("/")[2]) if config.SHOW_BRANCH == "TRUE" else ""
+    branch = (
+        (":" + content["ref"].split("/")[2])
+        if config.SHOW_BRANCH == "TRUE"
+        else ""
+    )
     commitUser = content['user_username']
 
     for commit in content["commits"]:
@@ -60,7 +70,11 @@ def main():
         if "githook:private" in commit['message']:
             commitMessage = "**This commit has been marked as private.**"
         else:
-            commitMessage = (commit['message'] if len(commit['message']) <= 50 else commit['message'][:47] + '...')
+            commitMessage = (
+                commit['message']
+                if len(commit['message']) <= 50
+                else commit['message'][:47] + '...'
+            )
         commitMessage = commitMessage.split('\n')[0]
         if authorHidden == False:
             commitMessage += " - **" + commitUser + "**"
@@ -75,17 +89,26 @@ def main():
             commits.append(
                 f"`{commit['id'][:7]}` - {commitMessage}"
             )
-
     if numOfCommits > 0:
-        data = {"embeds": [{"description": '\n'.join(map(str, commits)),
-                            "title": f"{numOfCommits} new commits on {repo}{branch}",
-                            "color": 14423100,
-                            "timestamp": datetime.now().isoformat()}]}
+        data = {
+            "embeds": [
+                {
+                    "description": '\n'.join(
+                        map(str, commits)
+                    ),
+                    "title": f"{numOfCommits} new commits on {repo}{branch}",
+                    "color": 14423100,
+                    "timestamp": datetime.now().isoformat(),
+                }
+            ]
+        }
 
-        r = requests.post(webhook_url,
-                          data=json.dumps(data),
-                          headers={'Content-Type': 'application/json'})
-    return (json.dumps(commits))
+        r = requests.post(
+            webhook_url,
+            data=json.dumps(data),
+            headers={'Content-Type': 'application/json'},
+        )
+    return json.dumps(commits)
 
 
 if __name__ == '__main__':
@@ -95,34 +118,45 @@ if __name__ == '__main__':
     try:
         import config
     except ImportError:
-        print("You have not setup your config correctly. Please rename configexample.py to config.py and fill out the values in the file.")
+        print(
+            "You have not setup your config correctly. Please rename configexample.py to config.py and fill out the values in the file."
+        )
         sys.exit(1)
-
     try:
         config.SHOW_BRANCH
     # Check if the user (somehow) forgot to add a SHOW_BRANCH key to the config.
     # We'll still let them run if this is missing, it's not like it's gonna ruin everything. Unlike you, Dave, you damn homewrecker.
     except AttributeError:
-        configerror("undefined", extra="You are missing a 'SHOW_BRANCH' key in your config. Branches will be hidden in commit embeds.")
+        configerror(
+            "undefined",
+            extra="You are missing a 'SHOW_BRANCH' key in your config. Branches will be hidden in commit embeds.",
+        )
         config.SHOW_BRANCH = "FALSE"
     else:
         # If SHOW_BRANCH is something weird, don't the branch in commit embed messages.
         if config.SHOW_BRANCH not in ("TRUE", "FALSE"):
-            configerror("undefined", extra="Your 'SHOW_BRANCH' key is neither 'TRUE' or 'FALSE'. Defaulting to 'FALSE'.")
+            configerror(
+                "undefined",
+                extra="Your 'SHOW_BRANCH' key is neither 'TRUE' or 'FALSE'. Defaulting to 'FALSE'.",
+            )
             config.SHOW_BRANCH = "FALSE"
-
     try:
         config.PORT
     # Check if the user (somehow) forgot to add a PORT key to the config.
     # We'll still let them run if this is missing, it's not like it's gonna ruin everything. Unlike Karen. Please I just want my kids back
     except AttributeError:
-        configerror("undefined", extra="You are missing a 'PORT' key in your config. Defaulting to port 5000.")
+        configerror(
+            "undefined",
+            extra="You are missing a 'PORT' key in your config. Defaulting to port 5000.",
+        )
         config.PORT = 5000
     else:
         # Same as above. We'll just set the default port ourselves cause we're a strong, independent application who don't need no user input
         # to find the port. *sassy fingersnap*
         if config.PORT == "":
-            configerror("undefined", extra="You are missing a 'PORT' value in your config. Defaulting to port 5000.")
+            configerror(
+                "undefined",
+                extra="You are missing a 'PORT' value in your config. Defaulting to port 5000.",
+            )
             config.PORT = 5000
-
     app.run(host='0.0.0.0', port=config.PORT)
