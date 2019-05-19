785f2fbe464075f6e7ee9646d047be434ee9c5af
doddsy <git@metrono.de>
2019-05-19 18:28:26 +0100

    :sparkles: :construction: :recycle: :alembic: Begin 'pluginization'
    For Githook's future it wouldn't have been great if every event was stuck in app.py - it would've gotten messy over time and near unmaintainable. This aims to make Githook run in a 'modular' way so that all events are in its own file and run dynamically when needed (if they exist).
    
    Big thanks to @byxor, without whom this would have been an absolute nightmare to figure out.

--- app.py
+++ app.py
@@ -1,24 +1,10 @@
 from flask import Flask, request, Response
 
-# Using json rather than Flask's jsonify because I couldn't figure it out and it made me wish to un-alive
-import json
 import requests
-from datetime import datetime
+from dispatch import dispatch
 import sys
-
-# Just wanted to try something new tbh
-def configerror(type, key=None, extra=None):
-    if type == "missing":
-        print(
-            f'You are missing the {key} key from your config.py. Please recreate the file.'
-        )
-    if type == "default":
-        print(
-            f'You are using the default {key} value in your config.py. Please change this before running the program.'
-        )
-    if type == "undefined":
-        print(extra)
-
+import importlib as imp
+import os
 
 def checkwebhook(url):
     r = requests.get(url).text
@@ -26,6 +12,17 @@ def checkwebhook(url):
     if all(keys in r for keys in ("name", "guild_id", "token")):
         return True
 
+file_names = os.listdir(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'plugins', ''))
+module_names = [f"plugins.{file_name[:-3]}" for file_name in file_names if os.path.isfile(
+    os.path.join(os.path.dirname(os.path.abspath(__file__)), 'plugins', '', file_name))]
+modules = [imp.import_module(module_name) for module_name in module_names]
+list_of_plugins = [module for module in modules if hasattr(module, 'EVENT')]
+
+plugins = {}
+for plugin in list_of_plugins:
+    plugins[plugin.EVENT] = plugin
+
+print(plugins)
 
 app = Flask(__name__)
 
@@ -36,9 +33,9 @@ def main():
     if 'webhook' in request.args:
         webhook_url = request.args.get('webhook')
         if checkwebhook(webhook_url) is not True:
-            return Response(status=400)
+            return Response(status=400, response="Your webhook URL is incorrect. Are you sure you entered it correctly?")
     else:
-        return Response(status=400)
+        return Response(status=400, response="You're missing a webhook URL!")
     # optional parameters
     # hideAuthor
     if 'hideAuthor' in request.args:
@@ -65,58 +62,20 @@ def main():
                     color = 14423100
             else:
                 color = 14423100
-    # If incoming request headers don't match Gitlab's headers
-    if not request.headers.get('X-Gitlab-Event'):
-        # 401 unauthorised
+    try:
+        plugin = plugins[request.headers.get('X-Gitlab-Event')]
+        data = plugin.run(request,
+                   webhook_url,
+                   authorHidden,
+                   branchHidden,
+                   color
+                   )
+        status = dispatch(data, webhook_url)
+        return Response(status=status)
+    except KeyError:
+        return Response(status=405, response="That event isn't supported by Githook (yet).")
+    except:
         return Response(status=401)
-    # Get incoming json data (GL will only POST JSON)
-    content = request.get_json()
-    commits = []
-    numOfCommits = 0
-    repo = content["repository"]["name"]
-    isPrivate = content["repository"]["visibility_level"]
-    branch = (":" + content["ref"].split("/")[2]) if branchHidden == False else ""
-    commitUser = content['user_username']
-
-    for commit in content["commits"]:
-        if "githook:ignore" in commit['message']:
-            continue
-        if "githook:private" in commit['message']:
-            commitMessage = "**This commit has been marked as private.**"
-        else:
-            commitMessage = (
-                commit['message']
-                if len(commit['message']) <= 50
-                else commit['message'][:47] + '...'
-            )
-        commitMessage = commitMessage.split('\n')[0]
-        if authorHidden == False:
-            commitMessage += " - **" + commitUser + "**"
-        commitUrl = commit['url']
-        numOfCommits += 1
-
-        if isPrivate != 0:
-            commits.append(f"[`{commit['id'][:7]}`]({commitUrl}) - {commitMessage}")
-        else:
-            commits.append(f"`{commit['id'][:7]}` - {commitMessage}")
-    if numOfCommits > 0:
-        data = {
-            "embeds": [
-                {
-                    "description": '\n'.join(map(str, commits)),
-                    "title": f"{numOfCommits} new commits on {repo}{branch}",
-                    "color": color,
-                    "timestamp": datetime.now().isoformat(),
-                }
-            ]
-        }
-
-        r = requests.post(
-            webhook_url,
-            data=json.dumps(data),
-            headers={'Content-Type': 'application/json'},
-        )
-    return json.dumps(commits)
 
 
 if __name__ == '__main__':
@@ -135,18 +94,16 @@ if __name__ == '__main__':
     # Check if the user (somehow) forgot to add a PORT key to the config.
     # We'll still let them run if this is missing, it's not like it's gonna ruin everything. Unlike Karen. Please I just want my kids back
     except AttributeError:
-        configerror(
-            "undefined",
-            extra="You are missing a 'PORT' key in your config. Defaulting to port 5000.",
+        print(
+            "You are missing a 'PORT' key in your config. Defaulting to port 5000.",
         )
         config.PORT = 5000
     else:
         # Same as above. We'll just set the default port ourselves cause we're a strong, independent application who don't need no user input
         # to find the port. *sassy fingersnap*
         if config.PORT == "":
-            configerror(
-                "undefined",
-                extra="You are missing a 'PORT' value in your config. Defaulting to port 5000.",
+            print(
+                "You are missing a 'PORT' value in your config. Defaulting to port 5000.",
             )
             config.PORT = 5000
     app.run(host='0.0.0.0', port=config.PORT)
